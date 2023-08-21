9249089384dde9733b87109fdb6e639e8f2e47bc
whirlpoolgreen <darkerhorn@gmail.com>
2023-08-21 22:42:54 +0900

    #4.4 createSlice

--- src/store.js
+++ src/store.js
@@ -1,12 +1,11 @@
 import { combineReducers } from "redux";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import { configureStore, createAction, createReducer } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const addToDo = createAction("ADD");
+/* const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-/* Redux Toolkit - createReducer */
 const reducer = createReducer([], (builder) => {
   builder
     .addCase(addToDo, (state, action) => {
@@ -15,14 +14,27 @@ const reducer = createReducer([], (builder) => {
     .addCase(deleteToDo, (state, action) =>
       state.filter((toDo) => toDo.id !== action.payload)
     );
+}); */
+
+const toDos = createSlice({
+  name: "toDosReducer",
+  initialState: [],
+  reducers: {
+    add: (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    },
+    remove: (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload),
+  },
 });
 
-export const actionCreators = {
-  addToDo,
-  deleteToDo,
-};
+export const { add, remove } = toDos.actions;
 
 /* redux persist */
+
+const rootReducer = combineReducers({
+  reducer: toDos.reducer,
+});
 
 // 새로운 persistConfig 선언
 const persistConfig = {
@@ -30,14 +42,16 @@ const persistConfig = {
   storage,
 };
 
-const rootReducer = combineReducers({
-  reducer,
-});
-
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
-const store = configureStore({ reducer: persistedReducer });
-const persistor = persistStore(store);
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+let persistor = persistStore(store);
 
 const stores = {
   store,
