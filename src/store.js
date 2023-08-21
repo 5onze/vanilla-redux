42ac83e9e1e1d26df0d91e053e7917c938dc3961
whirlpoolgreen <darkerhorn@gmail.com>
2023-08-21 16:53:09 +0900

    Redux Toolkit - #4.2 createReducer(Builder Callback 표기법)

--- src/store.js
+++ src/store.js
@@ -6,23 +6,15 @@ import { createAction, createReducer } from "@reduxjs/toolkit";
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
-/* const reducer = (state = [], action) => {
-  switch (action.type) {
-    case addToDo.type:
-      return [{ text: action.payload, id: Date.now() }, ...state];
-    case deleteToDo.type:
-      return state.filter((toDo) => toDo.id !== action.payload);
-    default:
-      return state;
-  }
-}; */
-
-const reducer = createReducer([], {
-  [addToDo]: (state, action) => {
-    state.push({ text: action.payload, id: Date.now() });
-  },
-  [deleteToDo]: (state, action) =>
-    state.filter((toDo) => toDo.id !== action.payload),
+/* Redux Toolkit - createReducer */
+const reducer = createReducer([], (builder) => {
+  builder
+    .addCase(addToDo, (state, action) => {
+      state.push({ text: action.payload, id: Date.now() });
+    })
+    .addCase(deleteToDo, (state, action) =>
+      state.filter((toDo) => toDo.id !== action.payload)
+    );
 });
 
 export const actionCreators = {
